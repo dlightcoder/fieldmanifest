3b23ef81f27421f2ab573d20b8c2b0e2911b010c
dlightcoder <fasfasoel@gmail.com>
2026-07-06 12:23:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Paste the "Web app" URL you get after deploying apps-script/Code.gs here.
 // It looks like: https://script.google.com/macros/s/AKfycb.../exec
-const API_URL = "https://script.google.com/macros/s/AKfycbx067hqveUlkXXe5C7amfzHrh3nTaDuqCfQvdiFRMebsSqOSxzAQp0v5VKObzN1TUaHaw/exechttps://script.google.com/macros/s/AKfycbx067hqveUlkXXe5C7amfzHrh3nTaDuqCfQvdiFRMebsSqOSxzAQp0v5VKObzN1TUaHaw/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbx067hqveUlkXXe5C7amfzHrh3nTaDuqCfQvdiFRMebsSqOSxzAQp0v5VKObzN1TUaHaw/exec";
